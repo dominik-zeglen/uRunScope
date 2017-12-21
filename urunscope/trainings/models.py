31e5487731095ce7c6a64046309ab359a7a0760f
Jakub Draganek <jakub.draganek@gmail.com>
2017-12-22 00:23:42 +0100

    Add track json field

--- urunscope/trainings/models.py
+++ urunscope/trainings/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.postgres.fields import JSONField
 
 
 class Training(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     distance = models.DecimalField(max_digits=3, decimal_places=2)  # km
     start_time = models.DateTimeField()
-    stop_time = models.DateTimeField()
-    duration = models.DurationField()
+    stop_time = models.DateTimeField(blank=True)
+    duration = models.DurationField(blank=True)
+    track = JSONField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
