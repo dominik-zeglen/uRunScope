2ae45edf70b2131ff03c262d378903139e2966f5
Jakub Draganek <jakub.draganek@gmail.com>
2017-12-22 21:07:53 +0100

    Fix file upload in form

--- urunscope/trainings/views.py
+++ urunscope/trainings/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import TrackForm
 
 
+@login_required
 def add(request):
     if request.method == 'POST':
-        form = TrackForm(request.POST)
+        form = TrackForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            return redirect(reverse('welcome'))
+            training = form.save(commit=False)
+            training.user = request.user
+            training.save()
+            messages.success(request, 'Training successfuly imported!')
+            return redirect('training:show', pk=training.pk)
+        else:
+            messages.warning(request, 'Incorrect training input!')
+            return render(request, 'training.html', {'form': form})
     form = TrackForm()
     return render(request, 'training.html', {'form': form})
