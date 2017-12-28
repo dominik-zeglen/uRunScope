8f586bf0d6428ef6e664dd3ec0552591f49bda4f
Jakub Draganek <jakub.draganek@gmail.com>
2017-12-28 01:03:45 +0100

    Add hrm summary model

--- urunscope/trainings/models.py
+++ urunscope/trainings/models.py
@@ -3,15 +3,24 @@ from django.conf import settings
 from django.contrib.postgres.fields import JSONField
 
 
+class HrmSummary(models.Model):
+    lowest = models.PositiveSmallIntegerField()
+    highest = models.PositiveSmallIntegerField()
+    avarage = models.PositiveSmallIntegerField()
+
+
 class Training(models.Model):
     title = models.CharField(max_length=50)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     distance = models.DecimalField(max_digits=5, decimal_places=2)  # km
     start_time = models.DateTimeField()
     stop_time = models.DateTimeField(blank=True)
     duration = models.DurationField(blank=True)
     track = JSONField(blank=True, null=True)
     hrm = JSONField(blank=True, null=True)
+    hrm_summary = models.OneToOneField(
+        HrmSummary, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.title
